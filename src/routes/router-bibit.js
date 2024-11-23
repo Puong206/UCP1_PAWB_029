eb1b26d578968ad46a3cb8896f719c59b5ad8e4e
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2024-11-23 15:22:42 +0700

    insert router-bibit

---
+++ src/routes/router-bibit.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const bibitController = require('../controllers/controller-bibit');
+
+router.get('/bibit', bibitController.getAllBibit);
+router.get('/bibit/add', bibitController.formAddBibit);
+router.post('/bibit/save', bibitController.saveBibit);
+router.get('/bibit/edit/:id', bibitController.formEditBibit);
+router.post('/bibit/edit/:id', bibitController.updateBibit);
+router.get('/bibit/delete/:id', bibitController.deleteBibit);
+
+module.exports = router;
