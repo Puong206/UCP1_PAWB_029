c39b2205936898d9a7ba6aeacd9bf6906606450e
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2024-11-23 15:52:54 +0700

    insert controllers/index.js

---
+++ src/controllers/index.js
@@ -0,0 +1,13 @@
+const homeController = require('./controller-home');
+const pupukController = require('./controller-pupuk');
+const bibitController = require('./controller-bibit');
+const loginController = require('./controller-login');
+const registerController = require('./controller-register');
+
+module.exports = {
+    homeController,
+    pupukController,
+    bibitController,
+    loginController,
+    registerController
+};
