a7f26b17e1117eab923f17a1079de7ea89b56f88
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2024-11-23 15:17:33 +0700

    insert router-pupuk

---
+++ src/routes/router-pupuk.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const pupukController = require('../controllers/controller-pupuk');
+
+router.get('/pupuk', pupukController.getAllPupuk);
+router.get('/pupuk/add', pupukController.formAddPupuk);
+router.post('/pupuk/save', pupukController.savePupuk);
+router.get('/pupuk/edit/:id', pupukController.formEditPupuk);
+router.get('/pupuk/edit/:id', pupukController.updatePupuk);
+router.get('/pupuk/delete/:id', pupukController.deletePupuk);
+
+module.exports = router;
